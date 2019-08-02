d859849e27b6367f047c3d396a8faaeac7f15d0e
Luke Becker <beckerluke1@gmail.com>
2019-08-02 16:32:57 -0500

    Added addEmployeeInfo function that takes value from input fields and places it in the individuallEmployeeInfo object, then pushes it to the allEmployeeInformation array

--- client.js
+++ client.js
@@ -1,1 +1,22 @@
+const allEmployeeInformation = [];
 
+
+$(document).ready(init); 
+
+function init() {
+    $('.js-btn-submit').on('click', addEmployeeInfo); 
+}
+
+
+function addEmployeeInfo() {
+    const individualEmployeeInfo = {
+        firstName: $('.js-input-firstName').val(), 
+        lastName: $('.js-input-lastName').val(),
+        idNumber: parseInt( $('.js-input-idNumber').val() ), 
+        title: $('.js-input-jobTitle').val(),
+        annualSalary: parseInt ($('.js-input-annualSalary').val() )
+    }
+
+    allEmployeeInformation.push(individualEmployeeInfo); 
+
+}
