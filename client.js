2a4aeb0a03c85680f5c5001aca46ed631794930e
Luke Becker <beckerluke1@gmail.com>
2019-08-02 16:54:33 -0500

    Added emptying to input fields after submit button is clicked

--- client.js
+++ client.js
@@ -4,19 +4,35 @@ const allEmployeeInformation = [];
 $(document).ready(init); 
 
 function init() {
+    render();  
     $('.js-btn-submit').on('click', addEmployeeInfo); 
 }
 
 
 function addEmployeeInfo() {
+
     const individualEmployeeInfo = {
         firstName: $('.js-input-firstName').val(), 
         lastName: $('.js-input-lastName').val(),
         idNumber: parseInt( $('.js-input-idNumber').val() ), 
         title: $('.js-input-jobTitle').val(),
-        annualSalary: parseInt ($('.js-input-annualSalary').val() )
+        annualSalary: parseInt( $('.js-input-annualSalary').val() )
     }
 
-    allEmployeeInformation.push(individualEmployeeInfo); 
+    $('.js-input-firstName').val(''); 
+    $('.js-input-lastName').val(''); 
+    $('.js-input-idNumber').val(''); 
+    $('.js-input-jobTitle').val(''); 
+    $('.js-input-annualSalary').val('');
+
+    allEmployeeInformation.push(individualEmployeeInfo);
+    console.log(allEmployeeInformation);
+
+    
+    
+
+}
+
+function render() {
 
 }
